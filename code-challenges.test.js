85a4feac094270268d99976ace0b3eefac615fb4
romeoyutig <romeoyutig2@outlook.com>
2023-02-01 08:17:30 -0800

    attempted assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,10 +1,11 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
 
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
 
-// Use test driven development to complete the following questions
+// Use test pon                                                                         hjdriven development to complete the following questions
 // Add appropriate dependencies: $ yarn add jest
 
 // Reminder: The test will call your function
@@ -19,10 +20,33 @@ const people = [
   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
   { name: "arthur dent", occupation: "a radio employee" }
 ]
-// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."]
+// Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."] 
+
+describe("sentenceMaker", () => {
+  it("returns an array with a sentence about each person with their name capitalized.", () => {
+    expect(sentenceMaker(people)).toEqual(["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is president of the galaxy.", "Arthur Dent is a radio employee."])
+  })
+})
+
+// ReferenceError: sentenceMaker is not defined
+// good test
 
 // b) Create the function that makes the test pass.
 
+// pseudo code: declare a function called sentenceMaker
+// input: array of objects
+// output: string
+// process: use .map() with the array, key and symbol as the arguments, 
+// return a string with the information provided in the test variable using string interpolation
+
+// const sentenceMaker = (arr) => {
+//   let firstPart = arr.map((obj => obj.name))
+//   let secondPart = arr.occupation
+//   return firstPart
+// }
+//   console.log(sentenceMaker(people))
+
+// Had a blocker here, I'm able to extract the obj, however it returns the array twice. I was planning on transforming each element into a string (.join)? , and then using .toUpperCase on index zero, then perhaps splicing the strings together, then using string interpolation and transform back into an array. also don't think i had the right arguments initially for my .map() function.
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
 
 // a) Create a test with an expect statement using the variables provided.
@@ -32,7 +56,29 @@ const hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 const hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+describe("remainders", () => {
+  it("returns an array of only the REMAINDERS of the numbers when divided by 3", () => {
+    expect(remainders(hodgepodge1)).toEqual([ 2, 0, -1, 0 ])
+  })
+})
+
+// ReferenceError: remainders is not defined
+// good test
+
+
 // b) Create the function that makes the test pass.
+
+// pseudo code:
+// input: an array of numbers, strings, and booleans
+// output: number (remainder of the numbers, when divided by 3)
+// use .filter() method to filter array
+// use modulo operator to find remainders of the numbers that are filtered.
+
+const remainders = (array) => {
+  let numberCheck = (array.filter(array => typeof array === "number"))
+    return numberCheck.map(num => num %3)
+  
+}
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
 
@@ -43,4 +89,32 @@ const cubeAndSum1 = [2, 3, 4]
 const cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+describe("cuber" , () => {
+  it("takes in an array of numbers and returns the sum of all the numbers cubed", () => {
+    expect(cuber(cubeAndSum1)).toEqual(99)
+  })
+})
+
+// ReferenceError: cuber is not defined
+// good test
+
+
+
 // b) Create the function that makes the test pass.
+
+// pseudo code: declare a function called cuber
+// input: array of numbers
+// output: number (sum of those numbers cubed)
+// process:use a for loop to iterate over the array one at a time, starting from 0 and stopping at the length of the parameter
+// have the arrays element cube itself (multiplying itself three times)
+// use the method .reduce() that takes in an an accumulator, and current value, as well as a initial value as arguments
+// return that method to run on the array provided as the cuber argument
+
+const cuber = (array) => {
+  for(let i = 0; i < array.length; i++) {
+    array[i] = array[i]*array[i]*array[i]
+  }
+  return array.reduce((a, b) => a + b, 0)
+}
+// console.log(cuber(cubeAndSum1))
+// console.log(cuber(cubeAndSum2))
--- interview-questions.md
+++ interview-questions.md
@@ -8,36 +8,65 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating a Rails application with a model called Cohort that has_many students, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be on the Cohort model or the Student model?
 
-Your answer:
+Your answer: I believe you can rename the columns with rename_column, and then migrate to update the database.
 
-Researched answer:
+Researched answer: If you forget a foreign key, you can rename the column using rename_column, or add a column with add_column. Then you can run a db:migrate to update the database.
 
 2. Which RESTful routes must always be passed params? Why?
 
-Your answer:
+Your answer: I believe show, edit, and update must always be passed params, because you are accessing a specific url.
 
-Researched answer:
+Researched answer: Destroy, show, edit, and update must always be passed params, because you are accessing a specific url.
 
 3. Name three rails generator commands. What is created by each?
 
-Your answer:
+Your answer: rails generate model will create a class with data inputs as the columns for our database. rails generate migration will create a new file to migrate. Stumped on a third generate command.
 
-Researched answer:
+Researched answer: rails generate model will create a class with data inputs as the columns for our database. rails generate migration will create a new file to migrate, this will update the schema. You can also use a rails generate controller command to setup a controller and create a a directory in your app.
 
 4. Consider the Rails routes below. What is the name of the controller method that would be called by each route? What action would each of the controller methods perform?
 
 action: "GET" location: /students
 
+#index is the method, and it would display all students
+
 action: "POST" location: /students
+
+#create is the controller method, and this will create a new student.
 
 action: "GET" location: /students/new
 
+#new is the controller method, and this will create a form to create a new student
+
 action: "GET" location: /students/2
+
+#show is the method, and this will display a specific student. 
 
 action: "GET" location: /students/2/edit
 
+#edit is the method, and this will display a form to update data for an existing student.
+
 action: "PATCH" location: /students/2
+
+#update is the method, and this will do the actual updating of the student.
 
 action: "DELETE" location: /students/2
 
+#destroy is the method, and this will delete a specific student.
+
 5. As a developer, you are making an application to manage your to do list. Create 10 user stories that will help you get your application started. Read more about [user stories](https://www.atlassian.com/agile/project-management/user-stories).
+
+
+As a user, I can fill out a form to add a task
+As a user, I can create that task and have it be displayed
+As a user, I can see all tasks that I've created on a landing page
+As a user, I can see an edit button to edit tasks
+As a user, I can delete existing tasks
+As a user, I can navigate from task to task
+As a user, I can sort tasks by start date
+As a user, I can prevent empty entries into task forms
+As a user, the page is simple, and styled nicely.
+As a user, I can search for a specific task.
+
+
+
